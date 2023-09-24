252a364edb169b8697275ec7be9022ed04e8a2b9
NeNeZoRr <zorrillalin@gmail.com>
2023-09-24 13:42:45 -0400

    Updated 	modified:   public/admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,68 +1,33 @@
 async function main() {
-  const root = document.querySelector("#root");
   const response = await fetch("http://localhost:3001/listBooks");
   const books = await response.json();
 
-  books.forEach((book) => {
-    const li = createBookItem(book);
+  const root = document.querySelector("#root");
 
-    const removeButton = li.querySelector(".remove-button");
-    removeButton.addEventListener("click", async () => {
-      await deleteBook(book.id);
+  books.forEach((book) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+          <span>${book.title}</span>
+          <input type="number" value="${book.quantity}">
+          <button>Save</button>
+      `;
+    const saveButton = li.querySelector("button");
+    const quantityInput = li.querySelector("input");
+
+    saveButton.addEventListener("click", async () => {
+      const updatedQuantity = quantityInput.value;
+      const updatedBook = { id: book.id, quantity: updatedQuantity };
+
+      await fetch("http://localhost:3001/updateBook", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedBook),
+      });
     });
+
     root.appendChild(li);
   });
-
-  const addButton = document.createElement("button");
-  addButton.textContent = "Add New Book";
-  addButton.addEventListener("click", () => {
-    const newBook = { title: "New Book", quantity: 0 };
-    createAndAddBook(newBook);
-  });
-  root.appendChild(addButton);
 }
-
-function createBookItem(book) {
-  const li = document.createElement("li");
-  li.innerHTML = `
-        <span>${book.title}</span>
-        <input type="number" value="${book.quantity}">
-        <button class="save-button">Save</button>
-        <button class="remove-button">Remove Book</button>
-    `;
-
-  const saveButton = li.querySelector(".save-button");
-  const quantityInput = li.querySelector("input");
-  saveButton.addEventListener("click", async () => {
-    const updatedQuantity = quantityInput.value;
-    const updatedBook = { id: book.id, quantity: updatedQuantity };
-    await updateBook(updatedBook);
-  });
-
-  return li;
-}
-
-async function createAndAddBook(newBook) {
-  try {
-    const response = await fetch("http://localhost:3001/addBook", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newBook),
-    });
-
-    if (response.ok) {
-      const createdBook = await response.json();
-      const li = createBookItem(createdBook);
-      const root = document.querySelector("#root");
-      root.appendChild(li);
-    } else {
-      console.error("Error adding book:", response.status);
-    }
-  } catch (error) {
-    console.error("Error adding book:", error);
-  }
-}
-
 main();
